2009905a54914774e758f36c62c3aca468969e65
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2019-05-14 22:08:43 +0200

    Izdelal model (brez testov)

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import random
+
 STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA, PONOVLJENA_CRKA, NAPACNA_CRKA = '+', 'o', '-'
 ZMAGA, PORAZ = 'W', 'X'
@@ -16,3 +18,48 @@ class Igra:
 
     def pravilne_crke(self):
         return list(set(self.crke) & set(self.geslo))
+
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
+
+    def zmaga(self):
+        return len(self.pravilne_crke()) == len(set(self.geslo))
+
+    def poraz(self):
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
+
+    def pravilni_del_gesla(self):
+        s = ['_']*len(self.geslo)
+        pravilne_crke = self.pravilne_crke()
+        for i, c in enumerate(self.geslo):
+            if c in pravilne_crke:
+                s[i] = c
+        return ''.join(s)
+
+    def nepravilni_ugibi(self):
+        return ' '.join(self.napacne_crke())
+
+    def ugibaj(self, c: str):
+        if c in self.crke:
+            return PONOVLJENA_CRKA
+        elif c in self.geslo:
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
+
+
+bazen_besed = list()
+with open("besede.txt", 'r') as fin:
+    bazen_besed = fin.read().split('\n')
+del bazen_besed[-1]
+# print(besede[-1])
+
+
+def nova_igra():
+    return Igra(geslo=random.choice(bazen_besed))
