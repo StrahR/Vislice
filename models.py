6bf40706f758d240fa0aa1719949646e23d60a7c
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2019-05-14 21:48:53 +0200

    Added class `Igra` with methods `napacne_crke` and `pravilne_crke`

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
 STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA, PONOVLJENA_CRKA, NAPACNA_CRKA = '+', 'o', '-'
 ZMAGA, PORAZ = 'W', 'X'
+
+ABECEDA_VELIKE = 'ABCČDEFGHIJKLMNOPQRSŠTUVWXYZŽ'
+ABECEDA_MALE = ABECEDA_VELIKE.lower()
+
+
+class Igra:
+    def __init__(self, geslo, crke):
+        self.geslo = geslo
+        self.crke = crke
+
+    def napacne_crke(self):
+        return list(set(self.crke) - set(self.geslo))
+
+    def pravilne_crke(self):
+        return list(set(self.crke) & set(self.geslo))
